e767e52642c1b9d15af3bebcd6139303a68b3534
Oirled <nclsdlr@gmail.com>
2016-01-28 17:29:31 +0100

    up eclairage

--- app/src/main/java/polytech/pfe_ndar/objdisplaytools/views/Activity3D.java
+++ app/src/main/java/polytech/pfe_ndar/objdisplaytools/views/Activity3D.java
@@ -2,13 +2,32 @@ package polytech.pfe_ndar.objdisplaytools.views;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.content.Intent;
-public class Activity3D extends Activity {
+
+
+import java.io.Console;
+
+import polytech.pfe_ndar.R;
+
+public class Activity3D extends AppCompatActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
+        Log.d("TTEST", "test");
+        Toolbar toolbar = new Toolbar(this);
+        toolbar.setTitle("Test");
+        setSupportActionBar(toolbar);
         this.startActivity( new Intent(this,Obj3DRenderer.class));
     }
+
 }
